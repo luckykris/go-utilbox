76890ec140517d1c01c28f5eef1f4107ab1d9c57
root <root@dev-zabbix-server-001.(none)>
2015-06-26 10:26:22 +0800

    asd

--- SignalHandle/SignalHandle.go
+++ SignalHandle/SignalHandle.go
@@ -3,6 +3,7 @@ import (
     "os"
     "os/signal"
     "syscall"
+    "runtime"
 )
 
 
@@ -11,6 +12,7 @@ func StartSignalHandle(signaltype string ,function func()){
 }
 
 func SignalHandle(signaltype string ,function func()){
+	runtime.Gosched()
 	for {
 		ch := make(chan os.Signal)
  		signal.Notify(ch, syscall.SIGINT, syscall.SIGUSR1, syscall.SIGUSR2,syscall.SIGHUP)
